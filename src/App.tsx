87bc26a0c372d21df4ccbb3e0a4da751c36aa2c3
shaiksaad-ux <shaiksaad72@gmail.com>
2026-07-09 15:04:48 +0530

    corrected routing for hotel owners

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import PostLogin from "@/(root)/PostLogin.tsx";
-
+import HotelDashboard from "@/(root)/HotelDashboard.tsx";
+import MyBookings from "@/(root)/MyBookings.tsx";
 
 
 
@@ -17,6 +18,8 @@ function App() {
             <Route path="/redirect" element={<PostLogin />} />
 
 
+            <Route path="/hotel-dashboard" element={<HotelDashboard />} />
+            <Route path="/bookings" element={<MyBookings />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     );
